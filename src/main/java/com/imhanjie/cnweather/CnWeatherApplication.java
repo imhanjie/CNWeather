278bbec9ca27e83de9981139119e8fb22d9fb946
hanjie <95hanjie@gmail.com>
2018-06-25 23:02:59 +0800

    init

---
+++ src/main/java/com/imhanjie/cnweather/CnWeatherApplication.java
@@ -0,0 +1,12 @@
+package com.imhanjie.cnweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnWeatherApplication.class, args);
+    }
+}
